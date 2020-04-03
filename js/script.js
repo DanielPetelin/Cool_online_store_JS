64dfe0c4463ef2ba06ec25630f42c62f49dc9ecc
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-03 09:34:13 +0300

    refact and add mustard

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@ var goods = {
         "name" : "Банан",
         "cost" : 30,
         "img" : "https://img.icons8.com/cotton/256/000000/sweet-banana.png",
+        "country" : "Россия",
         "sklad" : "Да"
     },
     "101" : {
         "name" : "Помидор",
         "cost" : 40,
         "img" : "https://img.icons8.com/cotton/256/000000/tomato--v2.png",
+        "country" : "Россия",
         "sklad" : "Нет"
     },
     "102" : {
         "name" : "Яблоко",
         "cost" : 35,
         "img" : "https://img.icons8.com/cotton/256/000000/apple--v2.png",
+        "country" : "Россия",
         "sklad" : "Да"
     }
 };
@@ -22,12 +25,25 @@ var goods = {
 console.log(goods);
 var out = '';
 
+out += '<div class="pricing-table row">'
 for (let key in goods) {
-    out += 'Название: ' + goods[key].name + '<br>';
-    out += 'Цена: ' + goods[key].cost + '<br>';
-    out += 'Наличие: ' + goods[key].sklad + '<br>';
-    out += '<img src="' + goods[key].img + '">'
+    out += '<div class="col col-md-6 col-lg-4">'
+    out += '<div class="package featured">'
+    out += '<p class="package-name">' + goods[key].name + '<p>';
+    out += '<img src="' + goods[key].img + '">';
     out += '<hr>'
+    out += '<p class="price">' + goods[key].cost + '₽' + '<p>';
+    out += '<p class="disclaimer">' + 'за штуку' + '</p>'
+    out += '<hr>'
+    out += '<ul class="features">'
+    out += '<li>' + 'Страна: ' + goods[key].country + '<br>';
+    out += '<li>' + 'Наличие: ' + goods[key].sklad + '<br>';
+    out += '</ul>'
+    out += '<hr>'
+    out += '<button class="button-primary">Buy</button>'
+    out += '</div>';
+    out += '</div>';
 };
+out += '</div>';
 
 document.querySelector('#out').innerHTML = out;
