63e2ab84e12504650c860992371313f9851dc299
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-03 08:45:20 +0300

    add new

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var goods = {
+    "100" : {
+        "name" : "Банан",
+        "cost" : 30,
+        "img" : "https://img.icons8.com/cotton/64/000000/sweet-banana.png",
+        "sklad" : "Да"
+    },
+    "101" : {
+        "name" : "Помидор",
+        "cost" : 20,
+        "img" : "https://img.icons8.com/cotton/64/000000/tomato--v1.png",
+        "sklad" : "Да"
+    }
+};
+
+console.log(goods);
